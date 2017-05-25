44e8cfe71b245a07fee6c0021de2dbf7b7b02e77
Pengy5934 <billy2000k@yahoo.com>
2017-05-24 22:59:33 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,8 +3,20 @@ import java.awt.*;
 
 public class Player
 {
-	public Player(int x, int y, Image img)
+	public Player(int x, int y, Image[] img)
 	{
 
 	}//end constructor
+	public void draw(Grpahics g)
+	{
+	}//end draw
+	public void walkLeft()
+	{
+	}//end run Left
+	public void walkRight()
+	{
+	}//end walk right
+	public void jump()
+	{
+	}//end jump
 }//end class
