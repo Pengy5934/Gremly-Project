b2ac51680b20e9adfee9efd0c173628b68e723fb
Pengy5934 <billy2000k@yahoo.com>
2017-05-31 18:27:41 -0400

    Update Platform.java

--- Platform.java
+++ Platform.java
@@ -7,21 +7,21 @@ public class Platform
 	Player player;
 	int x, y, height, width;
 	String type;
-	public Platform(int xIn, int yIn, int widthIn, int heightIn, Player playerImport)
+	public Platform(int xIn, int yIn, int widthIn, int heightIn)
 	{
 		x = xIn;
 		y = yIn;
 		width = widthIn;
 		height = heightIn;
-		player = playerImport;
 	}//end constructor
 	public void draw(Graphics g)
 	{
 		floor();
 		g.fillRect(x, y, height, width);
 	}//end draw
-	public void floor()
+	public void floor(Player playerImport)
 	{
+		player = playerImport;
 		if (player.getX() + player.getWidth() > x && player.getX() < x + width)
 		{
 			if (player.getY() + player.getHeight() >= y && player.getY() + player.getHeight() <= y + height)
@@ -31,18 +31,28 @@ public class Platform
 			}//end if
 		}//end if
 	}//end thrust
-	public void wall()
+	public void wall(Player playerImport)
 	{
+		player = playerImport;
 		if (player.getY() + player.getHeight() > y && player.getY() < y + height)
 		{
 			if (player.getX() <= x + width && player.getX() >= x)
 			{
 				player.stopLeft();
-				player.moveLeft((x + width)player.getX());
-			}//end if
-			if ()//above accounts for leftward motion, now do right
-			{
+				player.moveLeft((x + width) + player.getX());
 			}//end if
 		}//end if
 	}//end wall
+	public void ceiling(Player playerImport)
+	{
+		player = playerImport;
+		if (player.getX() + player.getWidth() > x && player.getX() < x + width)
+		{
+			if (player.getY() > y + height && player.getY() > y + height)
+			{
+				player.stopUp();
+				player.moveDown((y + height) - player.getY());
+			}//end if
+		}//end if
+	}//end ceiling
 }//end attack
