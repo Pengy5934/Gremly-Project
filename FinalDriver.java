cbdc9a751b69baed0078374324c28a14d7577b67
Pengy5934 <billy2000k@yahoo.com>
2017-06-01 14:02:43 -0400

    Update FinalDriver.java

--- FinalDriver.java
+++ FinalDriver.java
@@ -7,11 +7,22 @@ public class FinalDriver extends JApplet implements ActionListener, KeyListener
 	Timer time;
 	Player emily;
 	Player maggie;
+	Image maggieStand = getImage(getDocumentBase(), "maggieStand");
+	Image emilyStand = getImage(getDocumentBase(), "maggieStand");
+	Image [] emilyLeft = new Image[9];
+	Image [] maggieLeft = new Image[9];
 	public void init()
 	{
-		getContentPane(new DrawingPanel());
+		setContentPane(new DrawingPanel());
 		time = new Timer(10, this);
-		emily = new Player(50, 400, /*Image[]*/);
+
+		for (int i = 0; i < emilyLeft.length; i++)
+		{
+			emilyLeft[i] = getImage(getDocumentBase(), "");
+		}//end for loop
+
+		emily = new Player(50, 400, 50, 100, emilyStand);
+		maggie = new Player(900, 400, 50, 100, maggieStand);
 	}//end init
 	public class DrawingPanel extends JPanel
 	{
@@ -28,9 +39,12 @@ public class FinalDriver extends JApplet implements ActionListener, KeyListener
 	}//end key typed
 	public void keyPressed(KeyEvent e)
 	{
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			emily.jump();
+		}//end if
 	}//end key pressed
 	public void keyReleased(KeyEvent e)
 	{
 	}//end key released
 }//end public class
-
